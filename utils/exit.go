e3eba5808666b9636e0c7836ca0f23f3f2a2aaf9
Giorgos Komninos <admin@gkomninos.com>
2022-08-20 08:47:09 +0300

    passes a print function to exitRecover

--- utils/exit.go
+++ utils/exit.go
@@ -4,17 +4,21 @@ import (
 	"os"
 )
 
-//
+// Exit a struct that contains the ExitCode and message
 type Exit struct {
 	Code int
 	Msg  string
 }
 
 // ExitRecover should be used with a defer statement
-// See examples/todo/main.go for a sample usage
-func ExitRecover() {
+// fn is a function that accept a string. It should be used to print
+// the error message
+func ExitRecover(fn func(msg string)) {
 	if e := recover(); e != nil {
 		if exit, ok := e.(Exit); ok == true {
+			if fn != nil {
+				fn(exit.Msg)
+			}
 			os.Exit(exit.Code)
 		}
 		panic(e)
